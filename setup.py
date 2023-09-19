9661c6981a94f0c77aefa82b9c092c988839da03
Zaid _ <117419326+x72x@users.noreply.github.com>
2023-09-19 17:24:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '2.5'
+VERSION = '2023.9.19'
 DESCRIPTION = 'Pyrogram listener'
 
 # Setting up
